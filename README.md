dce45d6f13e6d5211ae377ea0846025ff844468b
Kevin Michael Schott <kevin@metab.rocks>
2022-01-21 10:23:09 +0100

    prepared for custom-layers

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# Random Image Generator
+# Random Image Generator (for NFTs)
 
 A crude tool for generating random NFT images and metadata.
 
-See it in action in the [Web3 NFT Tutorial](https://youtu.be/meTpMP0J5E8) on YouTube.
+See it in action in the [Web3 NFT Tutorial](https://youtu.be/meTpMP0J5E8) on YouTube. Forked from [fireship.io](https://github.com/fireship-io/nft-art-generator).
 
 ## Usage
 
-```
+```bash
 git clone <this-repo>
-npm install
-npm run randos
+yarn
+yarn dev
 ```
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { readFileSync, writeFileSync, readdirSync, rmSync, existsSync, mkdirSync } = require('fs');
-const sharp = require('sharp');
+const { readFileSync, writeFileSync, readdirSync, rmSync, existsSync, mkdirSync } = require('fs')
+const sharp = require('sharp')
 
 const template = `
     <svg width="256" height="256" viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -13,71 +13,65 @@ const template = `
     </svg>
 `
 
-const takenNames = {};
-const takenFaces = {};
-let idx = 999;
+const takenNames = {}
+const takenFaces = {}
+let idx = 999
 
 function randInt(max) {
-    return Math.floor(Math.random() * (max + 1));
+    return Math.floor(Math.random() * (max + 1))
 }
 
 function randElement(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
+    return arr[Math.floor(Math.random() * arr.length)]
 }
 
-
 function getRandomName() {
-    const adjectives = 'fired trashy tubular nasty jacked swol buff ferocious firey flamin agnostic artificial bloody crazy cringey crusty dirty eccentric glutinous harry juicy simple stylish awesome creepy corny freaky shady sketchy lame sloppy hot intrepid juxtaposed killer ludicrous mangy pastey ragin rusty rockin sinful shameful stupid sterile ugly vascular wild young old zealous flamboyant super sly shifty trippy fried injured depressed anxious clinical'.split(' ');
-    const names = 'aaron bart chad dale earl fred grady harry ivan jeff joe kyle lester steve tanner lucifer todd mitch hunter mike arnold norbert olaf plop quinten randy saul balzac tevin jack ulysses vince will xavier yusuf zack roger raheem rex dustin seth bronson dennis'.split(' ');
+    const adjectives = 'fired trashy tubular nasty jacked swol buff ferocious firey flamin agnostic artificial bloody crazy cringey crusty dirty eccentric glutinous harry juicy simple stylish awesome creepy corny freaky shady sketchy lame sloppy hot intrepid juxtaposed killer ludicrous mangy pastey ragin rusty rockin sinful shameful stupid sterile ugly vascular wild young old zealous flamboyant super sly shifty trippy fried injured depressed anxious clinical'.split(' ')
+    const names = 'aaron bart chad dale earl fred grady harry ivan jeff joe kyle lester steve tanner lucifer todd mitch hunter mike arnold norbert olaf plop quinten randy saul balzac tevin jack ulysses vince will xavier yusuf zack roger raheem rex dustin seth bronson dennis'.split(' ')
     
-    const randAdj = randElement(adjectives);
-    const randName = randElement(names);
-    const name =  `${randAdj}-${randName}`;
-
+    const randAdj = randElement(adjectives)
+    const randName = randElement(names)
+    const name =  `${randAdj}-${randName}`
 
     if (takenNames[name] || !name) {
-        return getRandomName();
+        return getRandomName()
     } else {
-        takenNames[name] = name;
-        return name;
+        takenNames[name] = name
+        return name
     }
 }
 
 function getLayer(name, skip=0.0) {
-    const svg = readFileSync(`./layers/${name}.svg`, 'utf-8');
+    const svg = readFileSync(`./layers/${name}.svg`, 'utf-8')
     const re = /(?<=\<svg\s*[^>]*>)([\s\S]*?)(?=\<\/svg\>)/g
-    const layer = svg.match(re)[0];
-    return Math.random() > skip ? layer : '';
+    const layer = svg.match(re)[0]
+    return Math.random() > skip ? layer : ''
 }
 
 async function svgToPng(name) {
-    const src = `./out/${name}.svg`;
-    const dest = `./out/${name}.png`;
-
-    const img = await sharp(src);
-    const resized = await img.resize(1024);
-    await resized.toFile(dest);
+    const src = `./out/${name}.svg`
+    const dest = `./out/${name}.png`
+    const img = await sharp(src)
+    const resized = await img.resize(1024)
+    await resized.toFile(dest)
 }
 
-
 function createImage(idx) {
-
-    const bg = randInt(5);
-    const hair = randInt(7);
-    const eyes = randInt(9);
-    const nose = randInt(4); 
-    const mouth = randInt(5);
-    const beard = randInt(3);
+    const bg = randInt(5)
+    const hair = randInt(7)
+    const eyes = randInt(9)
+    const nose = randInt(4) 
+    const mouth = randInt(5)
+    const beard = randInt(3)
     // 18,900 combinations
-
-    const face = [hair, eyes, mouth, nose, beard].join('');
+    const face = [hair, eyes, mouth, nose, beard].join('')
 
     if (face[takenFaces]) {
-        createImage();
+        createImage()
     } else {
         const name = getRandomName()
         console.log(name)
-        face[takenFaces] = face;
+        face[takenFaces] = face
 
         const final = template
             .replace('<!-- bg -->', getLayer(`bg${bg}`))
@@ -103,21 +97,17 @@ function createImage(idx) {
         writeFileSync(`./out/${idx}.svg`, final)
         svgToPng(idx)
     }
-
-
 }
-
 
 // Create dir if not exists
 if (!existsSync('./out')){
-    mkdirSync('./out');
+    mkdirSync('./out')
 }
 
 // Cleanup dir before each run
-readdirSync('./out').forEach(f => rmSync(`./out/${f}`));
-
+readdirSync('./out').forEach(f => rmSync(`./out/${f}`))
 
 do {
-    createImage(idx);
-    idx--;
-  } while (idx >= 0);
+    createImage(idx)
+    idx--
+} while (idx >= 0)
